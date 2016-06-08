14f007890f7aae9f71b17b25d0d97c1c2bb93a31
viktorzavadil <viktor.zavadil@optimal-development.cz>
2016-06-08 11:11:44 +0200

    Add less

--- src/app.config.js
+++ src/app.config.js
@@ -2,3 +2,7 @@ export function routing($urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
 }
+
+export default function () {
+  this.asdfasdp;
+}
